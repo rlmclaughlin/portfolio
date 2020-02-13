9ac7f243adc07b8920667d4f62845766a1d6c33e
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-12 18:35:46 -0600

    changed styling on pick em page, adjusted spacing and picture width

--- src/components/Projects/pickem.js
+++ src/components/Projects/pickem.js
@@ -13,37 +13,47 @@ export default class Pickem extends React.Component{
                   <hr className='hr'/>
                 </div> 
                 <div className='row-1'>
-                  <div>
-                    <h2>What is Pickem?</h2>
-                    <p>An investment application that provides real-time stock information.</p> 
-                    <a href='https://getpickem.co/'>Visit Site</a> 
-                  </div> 
                   <div className='showcase-image'>
                     <img alt='pickem website logo' className='pickem-img'></img> 
+                  </div>
+                  <div className='section-2'>
+                    <h2>What is Pickem?</h2>
+                    <p>Pick Em is a investor application that makes it easier to research stocks and make 
+                       better investment decisions. Our easy to use app provides both beginner and sophisticated
+                       investors with an automated system that analyzes key financial indicators. Pickem will 
+                       allow the customer to determine whether a buy signal is warranted.</p> 
+                    <a href='https://getpickem.co/'>Visit Site</a> 
                   </div> 
+
                 </div> 
                 <div className='showcase-hr'> 
                   <hr className='hr'/>
                 </div>
+
                 <div className='row-2'>
-                  <div>
+                  <div className='showcase-image-2'>
+                    <img alt='hey' className='pickem-img-2'/>
+                  </div>                 
+                  <div className='list'>
                     <h2>Built Using</h2>
-                    <ul>
-                      <li>HTML/CSS</li>
-                      <li>Javascript</li>
-                      <li>React</li>
-                      <li>Node.js</li> 
-                    </ul>
-                    <ul className='built-using-list'>
-                      <li>Stripe</li> 
-                      <li>Firebase</li>
-                      <li>Postgres</li>   
-                    </ul>
-                    <a href='https://github.com/Lambda-School-Labs/labspt2-investor-data-app'>Github Repo</a> 
-                  </div> 
-                  <div className='showcase-image'>
-                    <img alt='pickem website' className='pickem-img-2'></img> 
-                  </div> 
+                      <ul>
+                        <li>HTML/CSS</li> 
+                        <li>Javascript</li> 
+                        <li>React</li> 
+                      </ul>
+                      <div style={{paddingRight:"20px"}}> 
+                        <ul> 
+                          <li>Node.js</li> 
+                          <li>Stripe</li> 
+                          <li>Firebase</li> 
+                          <li>Postgres</li>
+                          <li>UI</li> 
+                        </ul> 
+                      </div> 
+                     <a style={{width: '100%', textAlign: 'center' }}href='https://github.com/Lambda-School-Labs/labspt2-investor-data-app'>Github Repo</a> 
+                     
+                    </div> 
+
                 </div>
                 <div className='showcase-hr'> 
                   <hr className='hr'/>
