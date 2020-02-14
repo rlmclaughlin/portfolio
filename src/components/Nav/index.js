58edd07177ed3330d7f44fbe4da21fdfa876912f
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-13 20:49:10 -0600

    fixed spacing on contact file

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -28,10 +28,8 @@ export default class Navigation extends React.Component{
                         <div className="bar bar2"></div>
                         <div className="bar bar3"></div> 
                      </div>  
-                      <h2>Ryan McLaughlin</h2>
-                      
+                      <h2>Ryan McLaughlin</h2>                     
                         <i className="far fa-envelope mobile-envelope"></i>
-                      
                     </div> 
                     <div>
                       <a href='https://www.linkedin.com/in/ryan-l-mclaughlin'>
@@ -39,10 +37,8 @@ export default class Navigation extends React.Component{
                       </a>
                       <a href='https://www.facebook.com/ryan.mclaughlin.9887'>
                         <i className="fab fa-facebook"></i>
-                      </a> 
-                      
-                        <i className="far fa-envelope desktop-envelope"></i>
-                      
+                      </a>                      
+                        <i className="far fa-envelope desktop-envelope"></i>                     
                       <a href='https://github.com/rlmclaughlin'>
                         <i className="fab fa-github"></i>
                       </a>
